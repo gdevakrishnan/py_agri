430ca1475ecc2ccf96416507c28ac6ce914af030
deva <gdevakrishnan700@gmail.com>
2023-03-08 13:30:11 +0530

    sign up page was completed successful

--- agri_app/urls.py
+++ agri_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name="home"),
     path('about/', views.about, name="about"),
     path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
     # path("room/", views.room, name="room"),
 ]
--- agri_app/views.py
+++ agri_app/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
 from . import views
 
 def index(request):
     return render(request, 'index.html')
 
-def signup(request):
-    return render(request, 'signup.html')
-
 def about(request):
     return render(request, 'about.html')
+
+def signup(request):
+    if request.method == 'POST':
+        name = request.POST['username'];
+        email = request.POST['email'];
+        password1 = request.POST['password1'];
+        password2 = request.POST['password2'];
+        
+        if password1 == password2:
+            user = User.objects.create_user(username=name, email=email, password=password1)
+            user.save()
+            messages.success(request,'Your account has been created successful')
+            return redirect('login')
+        
+        else:
+            messages.warning(request, 'Password Mismatching')
+            return redirect('signup')
+        
+    else:
+        form = CreateUserForm()
+        return render(request, 'signup.html', {'form': form})
+
+def login(request):
+    return render(request, 'login.html')
